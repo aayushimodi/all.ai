650d86e2345a7630dd92150b1bff6a0efa7ec91d
EpicAmart <amartyarr@gmail.com>
2020-08-16 01:59:06 -0700

    Added comprehensive as main python file and integrated with Riya

---
+++ adj_to_noun.py
@@ -0,0 +1,42 @@
+from nltk.corpus import wordnet as wn
+
+WN_ADJECTIVE = 'a'
+WN_ADJECTIVE_SATELLITE = 's'
+
+def convert_adj_to_noun(word):
+    from_pos = 'a'
+    to_pos = 'n'
+
+    synsets = wn.synsets(word, pos=from_pos)
+
+    # Word not found
+    if not synsets:
+        return []
+
+    # Get all lemmas of the word (consider 'a'and 's' equivalent)
+    lemmas = []
+    for s in synsets:
+        for l in s.lemmas():
+            if s.name().split('.')[1] == from_pos or from_pos in (WN_ADJECTIVE, WN_ADJECTIVE_SATELLITE) and s.name().split('.')[1] in (WN_ADJECTIVE, WN_ADJECTIVE_SATELLITE):
+                lemmas += [l]
+
+    # Get related forms
+    derivationally_related_forms = [(l, l.derivationally_related_forms()) for l in lemmas]
+
+    # filter only the desired pos (consider 'a' and 's' equivalent)
+    related_noun_lemmas = []
+
+    for drf in derivationally_related_forms:
+        for l in drf[1]:
+            if l.synset().name().split('.')[1] == to_pos or to_pos in (WN_ADJECTIVE, WN_ADJECTIVE_SATELLITE) and l.synset().name().split('.')[1] in (WN_ADJECTIVE, WN_ADJECTIVE_SATELLITE):
+                related_noun_lemmas += [l]
+
+    # Extract the words from the lemmas
+    words = [l.name() for l in related_noun_lemmas]
+    len_words = len(words)
+
+    # Build the result in the form of a list containing tuples (word, probability)
+    result = [w for w in set(words)]
+
+    # return all the possibilities sorted by probability
+    return result
